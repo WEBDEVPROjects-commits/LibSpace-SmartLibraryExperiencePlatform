20fee155e6791d47a0c411a7bf9672f9535dc8e6
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-02-10 19:49:28 +0530

    Successfull navigation after clicking on SignIn

--- SignupLoginComponents/Login.jsx
+++ SignupLoginComponents/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { HomeContext } from '../Context/Context.jsx'
 import { useContext } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 function Login() {
     const props=useContext(HomeContext);
+    const navigate=useNavigate();
     // localStorage.setItem("email",props.email) 
     // localStorage.setItem("password",props.password) 
   return (
@@ -57,13 +58,14 @@ function Login() {
         </div>
 
         <button
-          type="submit"
+          type="button"
           className="w-full bg-gray-900 text-white py-3 rounded-lg font-medium hover:bg-gray-800 transition duration-200 "
           onClick={() => {
-          
+            props.setIsLoggedIn(true);
+            navigate("/Dashboard")
           }}
         >
-        <Link to={(localStorage.getItem("email"==props.email)) && (localStorage.getItem("password"==props.password))?"/Dashboard":"/Login"}>Sign in</Link>
+        Sign in
         </button>
       </form>
       <div className="relative my-6">
@@ -75,7 +77,10 @@ function Login() {
         </div>
       </div>
 
-      <button className="w-full border border-gray-200 py-3 rounded-lg font-medium text-gray-700 hover:bg-gray-50 transition duration-200 flex items-center justify-center gap-2">
+      <button className="w-full border border-gray-200 py-3 rounded-lg font-medium text-gray-700 hover:bg-gray-50 transition duration-200 flex items-center justify-center gap-2" onClick={() => {
+          localStorage.setItem("email",props.email) 
+          localStorage.setItem("password",props.password)  
+      }}>
         <svg className="w-5 h-5" viewBox="0 0 24 24">
           <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
           <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
