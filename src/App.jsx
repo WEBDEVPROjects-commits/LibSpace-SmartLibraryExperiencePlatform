660b0b9c257ef18bb2a4b4c93bc0109871996979
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-02-10 19:50:39 +0530

    Added a protection route in app.jsx for dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,23 @@ import Login from '../SignupLoginComponents/Login.jsx';
 import Signup from '../SignupLoginComponents/Signup.jsx';
 import { useState } from 'react';
 import Dashboard from '../DashboardComponents/Dashboard.jsx';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute.jsx';
 function App() {
   const [email,setEmail]=useState("");
   const [password,setPassword]=useState("");
+  const [isLoggedIn,setIsLoggedIn]=useState(false);
   return (
     <>
     
-    <HomeContext.Provider value={{email,setEmail,password,setPassword}}>
+    <HomeContext.Provider value={{email,setEmail,password,setPassword,isLoggedIn,setIsLoggedIn}}>
       <BrowserRouter>
         <Routes>
             <Route path='/' element={<HomePage/>}></Route>
             <Route path="/Login" element={<Login/>}></Route>
             <Route path="/Signup" element={<Signup/>}></Route>
-            <Route path="/Dashboard" element={<Dashboard/>}></Route>
-          {/* <Route path='/Login' element={<HomePage/>}></Route> */}
-          {/* <Route path='/Dashboard' element={<Dashboard/>}></Route> */}
+            <Route element={<ProtectedRoute/>}>
+            <Route path="/Dashboard" element={<Dashboard/>}></Route> 
+            </Route>
         </Routes>
       </BrowserRouter>
     </HomeContext.Provider>
