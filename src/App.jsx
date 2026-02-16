421ec3eff1af1cad84624ce0ff1e5ff79f88d012
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-02-16 23:45:52 +0530

    Passed Fetches Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,71 @@
-import './App.css'
-import {HomeContext}  from '../Context/Context.jsx'
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
-import HomePage from '../HomePageComponents/HomePage.jsx'
-import Login from '../SignupLoginComponents/Login.jsx';
-import Signup from '../SignupLoginComponents/Signup.jsx';
-import { useEffect, useState,useMemo } from 'react';
-import Dashboard from '../DashboardComponents/Dashboard.jsx';
-import { Navigate } from 'react-router-dom';
-import ProtectedRoute2 from '../ProtectedRoute/ProtectedRoute2.jsx';
-import ProtectedRoute1 from '../ProtectedRoute/ProtectedRoute1.jsx';
-import Books from '../DashboardComponents/Books.jsx';
+import "./App.css";
+import { HomeContext } from "../Context/Context.jsx";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import HomePage from "../HomePageComponents/HomePage.jsx";
+import Login from "../SignupLoginComponents/Login.jsx";
+import Signup from "../SignupLoginComponents/Signup.jsx";
+import { useEffect, useState, useMemo } from "react";
+import Dashboard from "../DashboardComponents/Dashboard.jsx";
+import { Navigate } from "react-router-dom";
+import ProtectedRoute2 from "../ProtectedRoute/ProtectedRoute2.jsx";
+import ProtectedRoute1 from "../ProtectedRoute/ProtectedRoute1.jsx";
+import Books from "../DashboardComponents/Books.jsx";
 function App() {
-  const [isLoggedIn,setIsLoggedIn]=useState(() => {
-   return  localStorage.getItem("isLoggedIn")==="true"
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    return localStorage.getItem("isLoggedIn") === "true";
   });
-  const [searchedBook,setsearchedBook]=useState("");
-  const [api,setApi]=useState([]);
-
+  const [searchedBook, setsearchedBook] = useState("");
+  const [api, setApi] = useState([]);
   useEffect(() => {
-    const fetches=async () => {
-      const resp=await fetch(`https://openlibrary.org/search.json?q=${searchedBook.replace(" ","+")}`);
-      // const resp=await fetch('https://openlibrary.org/search.json');
-      const data=await resp.json();
-      console.log(data)
-      setApi(data.docs);
-    }
-    fetches();
-  },[])
-  useEffect(() => {   
-    localStorage.setItem("isLoggedIn",isLoggedIn)
-  },[isLoggedIn])
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+  }, [isLoggedIn]);
 
-//  const Contextvalue= useMemo(() => {
-//     return {
-//       email,
-//       setEmail,
-//       password,
-//       setPassword,
-//       isLoggedIn,
-//       setIsLoggedIn
-//     }
-//   })
+  const fetches = async (searchedBook) => {
+    const resp = await fetch(
+      `https://openlibrary.org/search.json?q=${searchedBook.replace(" ", "+")}`,
+    );
+    // const resp=await fetch('https://openlibrary.org/search.json');
+    const data = await resp.json();
+    console.log(data);
+    setApi(data.docs);
+  };
+
+  //  const Contextvalue= useMemo(() => {
+  //     return {
+  //       isLoggedIn,
+  //       setIsLoggedIn
+  //     }
+  //   })
   return (
     <>
-    <HomeContext.Provider value={{isLoggedIn,setIsLoggedIn,searchedBook,setsearchedBook,api}}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<ProtectedRoute1/>}>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="/Login" element={<Login/>}>
-            {/* <Route path="/" element={isLoggedIn?<Navigate to="/Dashboard" replace/>:<HomePage />}/>
+      <HomeContext.Provider
+        value={{
+          isLoggedIn,
+          setIsLoggedIn,
+          searchedBook,
+          setsearchedBook,
+          api,
+          fetches,
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route element={<ProtectedRoute1 />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/Login" element={<Login />}>
+                {/* <Route path="/" element={isLoggedIn?<Navigate to="/Dashboard" replace/>:<HomePage />}/>
             <Route path="/Login" element={isLoggedIn?<Navigate to="/Dashboard" replace/>:<Login/>}> */}
-          </Route>
-          </Route>
-            <Route path="/Signup" element={<Signup/>}></Route>
-            <Route element={<ProtectedRoute2/>}>
-            <Route path="/Dashboard" element={<Dashboard/>}></Route> 
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </HomeContext.Provider>
+              </Route>
+            </Route>
+            <Route path="/Signup" element={<Signup />}></Route>
+            <Route element={<ProtectedRoute2 />}>
+              <Route path="/Dashboard" element={<Dashboard />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </HomeContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
